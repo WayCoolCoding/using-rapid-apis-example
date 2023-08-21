92ae5607e60009dde0b70c64cdd67f9a46976af4
Master Johnson <75007025+WayCoolCoding@users.noreply.github.com>
2023-08-20 21:29:40 -0700

    Understanding APIs

--- example1/js/config.js
+++ example1/js/config.js
@@ -1,2 +1,2 @@
-const MY_API_KEY = 'paste your API key here';
+const MY_API_KEY = '7342cc954emsh08a7140a58c2489p1891a8jsn2791ed7b40a7';
 export { MY_API_KEY };
--- example1/js/main.js
+++ example1/js/main.js
@@ -1,4 +1,6 @@
-import { MY_API_KEY } from './config.js';
+// import { MY_API_KEY } from './config.js';
+
+const MY_API_KEY = '7342cc954emsh08a7140a58c2489p1891a8jsn2791ed7b40a7';
 
 fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=en", {
 	"method": "GET",
