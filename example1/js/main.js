da492bab4cb7b0e150773a7dcf43a14fe18edf7b
Master Johnson <75007025+WayCoolCoding@users.noreply.github.com>
2023-08-21 21:28:31 -0700

    Update main.js

--- example1/js/main.js
+++ example1/js/main.js
@@ -19,3 +19,20 @@ fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=en", {
 .catch(err => {
 	console.log(err);
 });
+
+// const url = 'https://quotes15.p.rapidapi.com/quotes/random/';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '7342cc954emsh08a7140a58c2489p1891a8jsn2791ed7b40a7',
+// 		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
